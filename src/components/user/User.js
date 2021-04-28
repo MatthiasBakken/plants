5e91d186ba546d3f39ff460f741af2b7ee4a439c
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-04-28 01:08:01 -0700

    cleaned up bugs with login and finished user

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,7 +1,13 @@
 import React from 'react'
+
+import { Link } from 'react-router-dom';
+
+import Plants from '../plants/Plants';
+
 import './user.scss'
 
 const jwtToken = localStorage.getItem( "jwtToken" );
+const loginMsg = localStorage.getItem( "loginMsg" );
 
 const User = () => {
 
@@ -9,10 +15,24 @@ const User = () => {
     return (
       <div className='container'>
         <h1>Water My Plants</h1>
-        <div>Stuff goes here</div>
+        <div>
+          {
+            loginMsg ?
+              <div>
+                <p>{`${loginMsg}`}</p>
+                <div className="plant-img-container">
+                  <img src="https://images.unsplash.com/photo-1520412099551-62b6bafeb5bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="plant picture" />
+                </div>
+              </div>
+              :
+              <p>Get started by <Link to="/create-plant">creating a plant</Link></p>
+          }
+        </div>
       </div>
     );
   } else {
     window.location.replace( '/login' );
   }
 }
+
+export default User;
