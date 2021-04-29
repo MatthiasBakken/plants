df4180f6b8e8db8e842619142d61993c398cf8cb
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-04-29 05:16:57 -0700

    styled the login component

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { Formik, Form, Field } from 'formik';
 import { Link } from 'react-router-dom';
 
-import Styles from './styles.scss'
+import './login.scss';
 
 
 const LoginSchema = Yup.object().shape( {
@@ -28,57 +28,59 @@ function Login ( props ) {
     }, [] );
 
     return (
-        <div className="form-container">
-            <Formik
-                initialValues={{
-                    username: '',
-                    password: '',
-                }}
-                validationSchema={LoginSchema}
-                onSubmit={( values ) => {
-                    axios.post( `https://tt157-backend.herokuapp.com/api/auth/login`, {
-                        username: values.username,
-                        password: values.password
-                    } ).then( res => {
-                        localStorage.setItem( "loginMsg", res.data.message );
-                        localStorage.setItem( "jwtToken", res.data.token );
-                        let jwtToken = localStorage.getItem( "jwtToken" );
-                        const parseJwt = ( token ) => {
-                            if ( !token ) {
-                                return;
-                            }
-                            const base64Url = token.split( '.' )[ 1 ];
-                            const base64 = base64Url
-                                .replace( '-', '+' )
-                                .replace( '_', '/' );
-                            return JSON.parse( window.atob( base64 ) );
-                        };
-                        const userId = parseJwt( jwtToken ).subject;
-                        localStorage.setItem( 'userId', userId );
-                        window.location.replace( '/' );
-                    } );
-                }}
-            >
-                {( { errors, touched, isValid, dirty } ) => (
-                    <Form className="form">
-                        <label >Username: </label>
-                        <Field name="username" />
-                        {errors.username && touched.username ? (
-                            <div className="error-div">{errors.username}</div>
-                        ) : null}
-                        <label>Password: </label>
-                        <Field name="password" type="password" />
-                        {errors.password && touched.password ? (
-                            <div className="error-div">{errors.email}</div>
-                        ) : null}
-                        
-                            <button type="submit" disabled={!( dirty && isValid )} >Log me in!</button>
-                        
-                    </Form>
-                )}
-            </Formik>
-            <div>
-                <p>Don't have an account? <Link to="/signup">Click Here</Link></p>
+        <div className="login-container">
+            <div className="form-container">
+                <Formik
+                    initialValues={{
+                        username: '',
+                        password: '',
+                    }}
+                    validationSchema={LoginSchema}
+                    onSubmit={( values ) => {
+                        axios.post( `https://tt157-backend.herokuapp.com/api/auth/login`, {
+                            username: values.username,
+                            password: values.password
+                        } ).then( res => {
+                            localStorage.setItem( "loginMsg", res.data.message );
+                            localStorage.setItem( "jwtToken", res.data.token );
+                            let jwtToken = localStorage.getItem( "jwtToken" );
+                            const parseJwt = ( token ) => {
+                                if ( !token ) {
+                                    return;
+                                }
+                                const base64Url = token.split( '.' )[ 1 ];
+                                const base64 = base64Url
+                                    .replace( '-', '+' )
+                                    .replace( '_', '/' );
+                                return JSON.parse( window.atob( base64 ) );
+                            };
+                            const userId = parseJwt( jwtToken ).subject;
+                            localStorage.setItem( 'userId', userId );
+                            window.location.replace( '/' );
+                        } );
+                    }}
+                >
+                    {( { errors, touched, isValid, dirty } ) => (
+                        <Form className="form">
+                            <label >Username: </label>
+                            <Field name="username" />
+                            {errors.username && touched.username ? (
+                                <div className="error-div">{errors.username}</div>
+                            ) : null}
+                            <label>Password: </label>
+                            <Field name="password" type="password" />
+                            {errors.password && touched.password ? (
+                                <div className="error-div">{errors.email}</div>
+                            ) : null}
+                            
+                                <button type="submit" disabled={!( dirty && isValid )} >Log me in!</button>
+                            
+                        </Form>
+                    )}
+                </Formik>
+                <div>
+                    <p>Don't have an account? <Link to="/signup">Click Here</Link></p>
+                </div>
             </div>
         </div>
     );
