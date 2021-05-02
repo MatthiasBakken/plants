86965d9c7e6d668584d597fee880b02b03741805
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-05-02 12:02:16 -0700

    ...

--- src/components/edit-plant/EditPlant.js
+++ src/components/edit-plant/EditPlant.js
@@ -6,14 +6,14 @@ import { useLocation } from 'react-router-dom';
 import './edit-plant.scss';
 
 
-const EditPlant = (props) => {
+const EditPlant = ( props ) => {
 
   const initialPlant = {
     nickname: '',
     species: '',
     h2o_frequency: '',
     image: ''
-  }
+  };
 
   const jwtToken = localStorage.getItem( "jwtToken" );
 
@@ -29,6 +29,7 @@ const EditPlant = (props) => {
     axios.get( `https://tt157-backend.herokuapp.com/api/plants/${plantId}` )
       .then( res => {
         setPlant( res.data );
+        return res.data;
       } );
   }, [] );
 
@@ -59,47 +60,43 @@ const EditPlant = (props) => {
       .catch( err => console.log( "cannot post plant", { err } ) );
   };
   
-  if ( jwtToken ) {
-    return (
-      <div className="edit-plant-container">
-        <form onSubmit={handleSubmit}>
-          <h2>Edit Your Plant</h2>
-          <input
-            type="text"
-            name="nickname"
-            placeholder="Nick Name"
-            onChange={changeHandler}
-            value={plant.nickname}
-          />
-          <input
-            type="text"
-            name="species"
-            placeholder="Species"
-            onChange={changeHandler}
-            value={plant.species}
-          />
-          <input
-            type="number"
-            name="h2o_frequency"
-            placeholder="H2oFrecuency"
-            onChange={changeHandler}
-            value={plant.h2o_frequency}
-          />
+  return (
+    <div className="edit-plant-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Edit Your Plant</h2>
+        <input
+          type="text"
+          name="nickname"
+          placeholder="Nick Name"
+          onChange={changeHandler}
+          value={plant.nickname}
+        />
+        <input
+          type="text"
+          name="species"
+          placeholder="Species"
+          onChange={changeHandler}
+          value={plant.species}
+        />
+        <input
+          type="number"
+          name="h2o_frequency"
+          placeholder="H2oFrecuency"
+          onChange={changeHandler}
+          value={plant.h2o_frequency}
+        />
     
-          <input
-            type="string"
-            name="image"
-            placeholder="Image URL"
-            onChange={changeHandler}
-            value={plant.image}
-          />
-          <button>Update Plant</button>
-        </form>
-      </div>
-    );
-  } else {
-    window.location.replace( '/login' );
-  }
-}
+        <input
+          type="string"
+          name="image"
+          placeholder="Image URL"
+          onChange={changeHandler}
+          value={plant.image}
+        />
+        <button>Update Plant</button>
+      </form>
+    </div>
+  );
+};
 
 export default EditPlant;
